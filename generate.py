f46e266c8c67354a438c1c859608b9ac76f01bc4
Sebas-Thomas <sothomas@uvm.edu>
2023-01-29 17:12:13 -0500

    Creating 5x5 tower group

--- generate.py
+++ generate.py
@@ -1,13 +1,33 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf")
+pyrosim.Start_SDF("tower.sdf")
 
-#length, width, height in size
+# default size variables
 length = 1
-width = 2
-height = 3
+width = 1
+height = 1
 
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[length, width, 
-height])
+# default position variables
+x = 0
+y = 0
+z = 0.5
+
+# for loop for tower
+for k in range(10):
+	pyrosim.Send_Cube(name="Box1", pos=[x,y,z] , size=[length, width, height])
+	# increase height by 1
+	z += 1
+			
+	# adding shrinking effect
+	length = length * 0.9
+	width = width * 0.9
+	height = height * 0.9
+	
+	for x in range(5):
+		pyrosim.Send_Cube(name="Box1", pos=[x,y,z] , size=[length, width, height])
+		x += 1
+		for y in range(5):
+			pyrosim.Send_Cube(name="Box1", pos=[x,y,z] , size=[length, width, height])
+			y += 1
 
 pyrosim.End()
