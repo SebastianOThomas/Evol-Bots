4d3f2a5c0d710bbdaba37c7354c87d83a3ed6102
Sebas-Thomas <sothomas@uvm.edu>
2023-01-29 16:07:50 -0500

    Added variables to generate length width and height to boxes

--- simulate.py
+++ simulate.py
@@ -1,8 +1,11 @@
 import pybullet as p
 import time
-
+import pybullet_data
 
 physicsClient = p.connect(p.GUI)
+
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
 # add gravity
 p.setGravity(0,0,-9.8)
 
@@ -11,7 +14,7 @@ planeId = p.loadURDF("plane.urdf")
 
 # import box
 p.loadSDF("box.sdf")
-for x in range(1000):
+for x in range(2000):
 	p.stepSimulation()
 	time.sleep(1/16)
 	print(x)
