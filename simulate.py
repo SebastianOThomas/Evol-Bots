9d4401e5482a9ee83f02f853fed388b7d5c62ebe
Sebas-Thomas <sothomas@uvm.edu>
2023-01-29 15:41:23 -0500

    Added gravity and Floor

--- simulate.py
+++ simulate.py
@@ -3,6 +3,13 @@ import time
 
 
 physicsClient = p.connect(p.GUI)
+# add gravity
+p.setGravity(0,0,-9.8)
+
+# add the floor
+planeId = p.loadURDF("plane.urdf")
+
+# import box
 p.loadSDF("box.sdf")
 for x in range(1000):
 	p.stepSimulation()
