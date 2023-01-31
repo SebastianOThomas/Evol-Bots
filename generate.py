7bd76e5e669e4c20a5ea8efe265b2f93db9e66f6
Sebas-Thomas <sothomas@uvm.edu>
2023-01-30 19:16:58 -0500

    Figuring out robot

--- generate.py
+++ generate.py
@@ -21,15 +21,19 @@ def Create_Robot():
 	pyrosim.Start_URDF("body.urdf")
 	
 	# FIRST LINK TORSO
-	pyrosim.Send_Cube(name="Link0", pos=[1.5,0,2.5] , size=[length, width, height])	
+	pyrosim.Send_Cube(name = "Torso", pos=[2,0,2] , size=[length, width, height])	
 	
 	# FIRST JOINT between TORSO and BACKLEG
-	pyrosim.Send_Joint(name = "Link0_Link1" , parent= "Link0" , child = "Link1" , type = "revolute", position = 
-[2,0,2])
+	pyrosim.Send_Joint(name = "Torso_Backleg" , parent= "Torso" , child = "Backleg" , type = "revolute", position = [1,0,1])
 	
 	# BACKLEG
-	pyrosim.Send_Cube(name="Link1", pos=[1.5,0,1.5] , size=[length, width, height])
+	pyrosim.Send_Cube(name = "Backleg", pos=[0,0,0] , size=[length, width, height])
 	
+	# JOINT BETWEEN TORSO AND FRONTLEG
+	pyrosim.Send_Joint(name = "Torso_Frontleg" , parent = "Torso" , child = "Frontleg" , type = "revolute", position = [1,0,0])
+
+	# FRONTLEG
+	pyrosim.Send_Cube(name = "Frontleg", pos=[2,0,1] , size=[length, width, height])
 	
 	pyrosim.End()
 
