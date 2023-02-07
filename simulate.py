42a032c8bc9b55e7ad8b724901e7725c1f75f12b
Sebas-Thomas <sothomas@uvm.edu>
2023-02-07 18:56:19 -0500

    Got bot to slide back and forth

--- simulate.py
+++ simulate.py
@@ -4,6 +4,7 @@ import pybullet_data
 import pyrosim.pyrosim as pyrosim
 import numpy
 import os
+import random as random
 
 physicsClient = p.connect(p.GUI)
 
@@ -22,24 +23,37 @@ robotId = p.loadURDF("body.urdf")
 pyrosim.Prepare_To_Simulate(robotId)
 
 # backleh numpy vector
-backLegSensorValues = numpy.zeros(100)
+backLegSensorValues = numpy.zeros(1000)
 
 # frontleh numpy vector
-frontLegSensorValues = numpy.zeros(100)
+frontLegSensorValues = numpy.zeros(1000)
 
 
 # import box
 p.loadSDF("world.sdf")
-for i in range(100):
+for i in range(1000):
 	p.stepSimulation()
-	
+
 	#backleg sensor
 	backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
 
 	#frontleg sensor
 	frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
 
-	time.sleep(1/16)
+	# motors for joints
+	#backleg
+	# 1.57079632679 ==  pi/2.0 & 
+	# -1.57079632679 == -pi/2.0
+	pyrosim.Set_Motor_For_Joint(bodyIndex = robotId, jointName = b'Torso_BackLeg', 
+	controlMode = p.POSITION_CONTROL, targetPosition = random.uniform(-1.57079632679, 1.57079632679), maxForce = 25)
+
+	#front leg
+	# 1.57079632679 ==  pi/2.0 & 
+	# -1.57079632679 == -pi/2.0
+	pyrosim.Set_Motor_For_Joint(bodyIndex = robotId, jointName = b'Torso_FrontLeg', 
+	controlMode = p.POSITION_CONTROL, targetPosition = random.uniform(-1.57079632679, 1.57079632679), maxForce = 25)
+	
+	time.sleep(1/10)
 	print(backLegSensorValues)
 
 #save values to file
