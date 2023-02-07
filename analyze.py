10c9ed8f1cbe818a44ffb6c30fc4d514fd9d7773
Sebas-Thomas <sothomas@uvm.edu>
2023-02-06 23:25:23 -0500

    Finsihed module F

--- analyze.py
+++ analyze.py
@@ -1,1 +1,22 @@
-#hello analyze
+import numpy
+import matplotlib.pyplot as plot
+import os
+
+# load files into vector
+backLegSensorValues = numpy.zeros(100)
+frontLegSensorValues = numpy.zeros(100)
+
+backLegSensorValues = numpy.load(os.path.join("data", "backLegSensorValues.npy"))
+frontLegSensorValues = numpy.load(os.path.join("data", "frontLegSensorValues.npy"))
+
+print(backLegSensorValues)
+print(frontLegSensorValues)
+
+#graph stuff
+plot.plot(backLegSensorValues, label = "Back Leg", linewidth = 5)
+plot.plot(frontLegSensorValues, label = "Front Leg")
+
+#legends
+plot.legend(loc = "upper right")
+#graph
+plot.show()
--- simulate.py
+++ simulate.py
@@ -1,6 +1,9 @@
 import pybullet as p
 import time
 import pybullet_data
+import pyrosim.pyrosim as pyrosim
+import numpy
+import os
 
 physicsClient = p.connect(p.GUI)
 
@@ -15,11 +18,34 @@ planeId = p.loadURDF("plane.urdf")
 #add the robot
 robotId = p.loadURDF("body.urdf")
 
+# additional pyrosim setup
+pyrosim.Prepare_To_Simulate(robotId)
+
+# backleh numpy vector
+backLegSensorValues = numpy.zeros(100)
+
+# frontleh numpy vector
+frontLegSensorValues = numpy.zeros(100)
+
+
 # import box
 p.loadSDF("world.sdf")
-for x in range(2000):
+for i in range(100):
 	p.stepSimulation()
+	
+	#backleg sensor
+	backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+
+	#frontleg sensor
+	frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
+
 	time.sleep(1/16)
-	print(x)
+	print(backLegSensorValues)
+
+#save values to file
+numpy.save(os.path.join("data", "backLegSensorValues.npy"), backLegSensorValues)
+numpy.save(os.path.join("data", "frontLegSensorValues.npy"), frontLegSensorValues)
 
 p.disconnect()
+
+
