7c7a1c2775dd75d230714649ced63255c33442bc
Sebas-Thomas <sothomas@uvm.edu>
2023-03-06 13:45:43 -0500

    Got seperate simulations to work

--- generate.py
+++ generate.py
@@ -1,4 +1,5 @@
 import pyrosim.pyrosim as pyrosim
+import random
 
 # default size variables
 length = 1
@@ -50,14 +51,15 @@ def Generate_Brain():
 	pyrosim.Send_Motor_Neuron( name = 3 , jointName = "Torso_BackLeg")
 	pyrosim.Send_Motor_Neuron( name = 4 , jointName = "Torso_FrontLeg")
 
-	# generate synapse
-	pyrosim.Send_Synapse(sourceNeuronName = 0 , targetNeuronName = 3 , weight = 1.0)
-	pyrosim.Send_Synapse(sourceNeuronName = 1 , targetNeuronName = 3 , weight = 1.0)
+	# generate synapse with a for loop
+	# pyrosim.Send_Synapse(sourceNeuronName = 0 , targetNeuronName = 3 , weight = 1.0)
+	# pyrosim.Send_Synapse(sourceNeuronName = 1 , targetNeuronName = 3 , weight = 1.0)
 
-	pyrosim.Send_Synapse(sourceNeuronName = 0 , targetNeuronName = 4 , weight = -1.0)
-	pyrosim.Send_Synapse(sourceNeuronName = 1 , targetNeuronName = 4 , weight = -1.0)
-
-
+	# pyrosim.Send_Synapse(sourceNeuronName = 0 , targetNeuronName = 4 , weight = -1.0)
+	# pyrosim.Send_Synapse(sourceNeuronName = 1 , targetNeuronName = 4 , weight = -1.0)
+	for i in range(3):
+		for j in range(3,5):
+			pyrosim.Send_Synapse(sourceNeuronName = i, targetNeuronName = j, weight = random.randrange(-1,1))
 	pyrosim.End()
 	
 link_size = [1, 1, 1]
